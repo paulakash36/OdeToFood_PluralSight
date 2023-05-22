b0918b65e97e9d2eaffab4699c1ec106a15dbcad
Akash Paul <paulakash36@gmail.com>
2023-05-22 06:52:20 +0530

    Handling Add/Update Logic

--- Pages/Restaurants/Edit.cshtml.cs
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -54,6 +54,7 @@ namespace OdeToFood.Pages.Restaurants
                 restaurantData.Add(Restaurant);
             }
             restaurantData.Commit();
+            TempData["Message"] = "Restaurant Saved!";
             return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
 
         }
