06530ea70f8cd8817946b82aaf4aaf17954fce69
Akash Paul <paulakash36@gmail.com>
2023-07-13 03:51:05 +0530

    Building the UI

---
+++ ViewComponent/RestaurantCountViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OdeToFood.DataAccessLayer;
+
+namespace OdeToFood.ViewComponents
+{
+    public class RestaurantCountViewComponent : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestaurantCountViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetRestaurantCount();
+            return View(count);
+        }
+    }
+}
